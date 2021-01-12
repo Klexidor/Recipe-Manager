3883837826cf760d3f75c671c530cafa081b2df9
Maikel <m.vdvelden@hotmail.com>
2021-01-12 13:19:55 +0100

    Added domain

---
+++ Recipe Manager/Recipe Manager/Recipe Manager/Domain/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Recipe_Manager.Domain
+{
+	public class Recipe
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Category { get; set; }
+		public string Area { get; set; }
+		public string Instructions { get; set; }
+		public string ThumbnailUrl { get; set; }
+		public string Tags { get; set; } //ToDo: change into string array for future refined tag search
+		public string VideoUrl { get; set; }
+		public Dictionary<string, string> IngredientsAndMeasurements { get; set; }
+		public string Source { get; set; }
+		public string DateModified { get; set; }
+	}
+}
